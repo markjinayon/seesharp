fe8c4eaecb5601ec8a4159713f6985446a088d59
Jezreel Bitgue <jembitgue8@gmail.com>
2022-06-13 00:15:43 +0800

    adjust ui in item lists xml modified notifs to reports added logout xml added reports ui

---
+++ app/src/main/java/com/parentalcontrol/seesharp/activities/parent/ReportsActivity.java
@@ -0,0 +1,14 @@
+package com.parentalcontrol.seesharp.activities.parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reports);
+    }
+}
